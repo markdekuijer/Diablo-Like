997c6321c147ac760cd76c8f54b0fed01bb25e5b
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2018-06-14 23:13:46 +0200

    2 more particles added and root abbility

---
+++ GameplayP8/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("On Startup")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 pos1;
+    [SerializeField] private Vector3 pos2;
+
+    [Header("Movementspeed Effects")]
+    public bool isRooted;
+    public bool isSlowed;
+    public float slowDuration;
+    public float slowAmount;
+
+    private float originalSpeed;
+
+    private void Start()
+    {
+        originalSpeed = speed;
+        agent.SetDestination(pos1);
+    }
+
+
+    void Update ()
+    {
+        WaypointWalking();
+        HandleMovementSpeed();
+        HandleRoot();
+    }
+
+    public void GiveSlow(float amount, float duration)
+    {
+        isSlowed = true;
+        slowDuration = duration;
+        slowAmount = amount;
+    }
+
+    public void HandleMovementSpeed()
+    {
+        if (isSlowed)
+        {
+            slowDuration -= Time.deltaTime;
+            if (slowDuration <= 0)
+            {
+                isSlowed = false;
+                speed = originalSpeed;
+            }
+            else
+            {
+                speed = originalSpeed - slowAmount;
+            }
+
+            agent.speed = speed;
+        }
+    }
+
+    public void WaypointWalking()
+    {
+        if (transform.position == pos1)
+        {
+            agent.SetDestination(pos2);
+            print("a");
+        }
+        if (transform.position == pos2)
+        {
+            agent.SetDestination(pos1);
+            print("b");
+        }
+    }
+
+    public void HandleRoot()
+    {
+        if (isRooted)
+        {
+            agent.speed = 0;
+            agent.velocity = Vector3.zero;
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.speed = speed;
+            agent.isStopped = false;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//nice meme
---
+++ GameplayP8/Assets/Scripts/Player/Archer/ArcherAoeRootProtectionSkill.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherAoeRootProtectionSkill : AbbilitySkill
+{
+    [SerializeField] private List<GameObject> particles = new List<GameObject>();
+    [SerializeField] private float duration;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask mask;
+    [SerializeField] private float damage;
+    [SerializeField] private float protectionDebuff;
+
+    private List<EnemyMovement> enemys = new List<EnemyMovement>();
+    private float maxDuration;
+
+    private void Start()
+    {
+        maxDuration = duration;
+    }
+
+    public override void Init(Vector3 position = default(Vector3))
+    {
+        enemys.Clear();
+        Collider[] hitColliders = Physics.OverlapSphere(position, radius, mask);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            hitColliders[i].gameObject.GetComponent<HealthManager>().Damage(damage);
+            enemys.Add(hitColliders[i].gameObject.GetComponent<EnemyMovement>());
+            if(i < 25)
+            {
+                particles[i].gameObject.SetActive(true);
+                particles[i].gameObject.transform.position = hitColliders[i].gameObject.transform.position;
+            }
+        }
+
+        for (int i = 0; i < enemys.Count; i++)
+        {
+            enemys[i].isRooted = true;
+        }
+        duration = maxDuration;
+
+        //TODO reduce protections
+    }
+
+    public override void Tick()
+    {
+        if(duration >= 0)
+        {
+            duration -= Time.deltaTime;
+            if(duration < 0)
+            {
+                for (int i = 0; i < enemys.Count; i++)
+                {
+                    enemys[i].isRooted = false;
+                    particles[i].gameObject.SetActive(false);
+                }
+
+                //regain protections
+            }
+        }
+    }
+}
